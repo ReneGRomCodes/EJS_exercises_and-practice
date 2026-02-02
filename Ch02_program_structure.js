8358b848a574f08cf84d396dbe4833dab9990599
ReneGRomCodes <renegromcodes@gmail.com>
2026-02-02 18:01:04 +0100

    Added solutions for chapter 2 exercises.

--- Ch02_program_structure.js
+++ Ch02_program_structure.js
@@ -64,3 +64,15 @@ Passing this string to console.log should show something like this:
 When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works
 for any size, outputting a grid of the given width and height.
  */
+
+const size = 8;
+const oddRow = (" " + "# ".repeat(size / 2)).slice(0, size);
+const evenRow = ("#" + " #".repeat(size / 2)).slice(0, size);
+
+for (let i = 1; i <= size; i++) {
+    if (i % 2 === 0) {
+        console.log(evenRow);
+    } else {
+        console.log(oddRow);
+    }
+}
